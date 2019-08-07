db41324b64dc3e7ab7c6028386865f13425d4932
mcecchi <mauro.cecchi@met-informatica.it>
2019-08-07 17:28:46 +0200

    Integrate foosel/wifi_ap and rockymeza/wifi

--- netconnectd/server.py
+++ netconnectd/server.py
@@ -9,7 +9,7 @@ import subprocess
 import sys
 import time
 import threading
-from .wifi_ap import wifi_ap, Hostapd, Dnsmasq, AccessPoint
+from .wifi_ap import *, Hostapd, Dnsmasq, AccessPoint
 # import wifi
 # import wifi.scheme
 # import wifi.utils
